c5980baf877b9cc48a2e112b07be80303dc5f8ad
Justin Rich <justin@zebramm.com>
2011-01-20 21:49:03 -0500

    starting fresh screen scraping from credits up

--- lib/imdb/movie.rb
+++ lib/imdb/movie.rb
@@ -17,87 +17,24 @@ module Imdb
       @url = "http://www.imdb.com/title/tt#{imdb_id}/"
       @title = title.gsub(/"/, "") if title
     end
-    
-    # Returns an array with cast members
-    def cast_members
-      document.search("table.cast td.nm a").map { |link| link.innerHTML.strip.imdb_unescape_html } rescue []
-    end
-    
-    def cast_member_ids
-      document.search("table.cast td.nm a").map {|l| l['href'].sub(%r{^/name/(.*)/}, '\1') }
-    end
-    
-    # Returns the name of the director
-    def director
-      # document.at("h5[text()='Director:'] ~ a").innerHTML.strip.imdb_unescape_html rescue nil
-      document.search("h5[text()^='Director'] ~ a").map { |link| link.innerHTML.strip.imdb_unescape_html } rescue []
-    end
-    
-    # Returns an array of genres (as strings)
-    def genres
-      document.search("h5[text()='Genre:'] ~ a[@href*=/Sections/Genres/']").map { |link| link.innerHTML.strip.imdb_unescape_html } rescue []
-    end
 
-    # Returns an array of languages as strings.
-    def languages
-      document.search("h5[text()='Language:'] ~ a[@href*=/Sections/Languages/']").map { |link| link.innerHTML.strip.imdb_unescape_html } rescue []
-    end
-    
-    # Returns the duration of the movie in minutes as an integer.
-    def length
-      document.search("//h5[text()^='Runtime']/..").innerHTML[/\d+ min/].to_i rescue nil
-    end
-    
-    # Returns a string containing the plot.
-    def plot
-      sanitize_plot(document.search("h5[text()='Plot:'] ~ div").first.innerHTML) rescue nil
-    end
-    
-    # Returns a string containing the URL to the movie poster.
-    def poster
-      src = document.at("a[@name='poster'] img")['src'] rescue nil
-      case src
-      when /^(http:.+@@)/
-        $1 + '.jpg'
-      when /^(http:.+?)\.[^\/]+$/
-        $1 + '.jpg'
-      end
-    end
-    
-    # Returns a float containing the average user rating
-    def rating
-      document.at(".starbar-meta b").innerHTML.strip.imdb_unescape_html.split('/').first.to_f rescue nil
-    end
-    
-    # Returns a string containing the tagline
-    def tagline
-      document.search("h5[text()='Tagline:'] ~ div").first.innerHTML.gsub(/<.+>.+<\/.+>/, '').strip.imdb_unescape_html rescue nil
-    end
-    
-    # Returns a string containing the mpaa rating and reason for rating
-    def mpaa_rating
-      document.search("h5[text()='MPAA:'] ~ div").first.innerHTML.strip.imdb_unescape_html rescue nil
-    end
-    
-    # Returns a string containing the title
-    def title(force_refresh = false)
-      if @title && !force_refresh
-        @title
-      else
-        @title = document.at("h1").innerHTML.split('<span').first.strip.imdb_unescape_html rescue nil 
-      end
-    end
-    
-    # Returns an integer containing the year (CCYY) the movie was released in.
-    def year
-      document.search('a[@href^="/year/"]').innerHTML.to_i
-    end
-    
-    # Returns release date for the movie.
-    def release_date
-      sanitize_release_date(document.search('h5[text()*=Release Date]').first.next_sibling.innerHTML.to_s) rescue nil
-    end
+    def credits
+      raw_credits = (document/'table[@cellspacing="1"]')
 
+      raw_credits.inject({}) do |parsed_credits, result|
+        parsed_credits << case result.inner_text    
+        when /^Directed by.*/i, /Makeup/, /Art/
+          nil
+        when /^(Writing credits\s*)(.*)/i
+          {:written_by => $2.gsub(/\(written by\)/, '').gsub(/\s*and\s*/, ',').gsub(/^\(WGA\)\s*/, '').strip}
+        when /^(Cinematography by\s*)(.*)/i
+          {:cinematographer => $2.gsub(/\(director of photography\)/, '').gsub(/\s*and\s*/, ', ').strip}
+        else
+          nil
+        end
+      end            
+    end
+    
     private
     
     # Returns a new Hpricot document for parsing.
@@ -105,40 +42,14 @@ module Imdb
       @document ||= Hpricot(Imdb::Movie.find_by_id(@id))
     end
     
-    # Use HTTParty to fetch the raw HTML for this movie.
+    # Use HTTParty to fetch the raw HTML for this movie.  
     def self.find_by_id(imdb_id)
-      open("http://www.imdb.com/title/tt#{imdb_id}/")
+      open("http://www.imdb.com/title/tt#{imdb_id}/fullcredits")      
     end
     
     # Convenience method for search
     def self.search(query)
       Imdb::Search.new(query).movies
-    end
-
-    def self.top_250
-      Imdb::Top250.new.movies
-    end
-    
-    def sanitize_plot(the_plot)
-      the_plot = the_plot.imdb_strip_tags
-                                   
-      the_plot = the_plot.gsub(/add\ssummary|full\ssummary/i, "")
-      the_plot = the_plot.gsub(/add\ssynopsis|full\ssynopsis/i, "")
-      the_plot = the_plot.gsub(/&nbsp;|&raquo;/i, "")
-      the_plot = the_plot.gsub(/see|more/i, "")
-      the_plot = the_plot.gsub(/\|/i, "")
-      
-      the_plot = the_plot.strip.imdb_unescape_html
-    end
-    
-    def sanitize_release_date(the_release_date)
-      the_release_date = the_release_date.gsub(/<a.*a>/,"")
-      the_release_date = the_release_date.gsub(/&nbsp;|&raquo;/i, "")
-      the_release_date = the_release_date.gsub(/see|more/i, "")
-      
-      the_release_date = the_release_date.strip.imdb_unescape_html
-    end
-    
+    end    
   end # Movie
-  
 end # Imdb
